02c89cc27ea90477e5d1005477685f921760b1d7
kyuwonlee5 <75664301+kyuwonlee5@users.noreply.github.com>
2023-09-14 13:05:25 +0900

    [unrated] Title: 수열과 구간 쿼리 2, Time: 0.31 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, vector<vector<int>> queries) {
+    vector<int> answer;
+    for (int i = 0; i < queries.size(); ++i) {
+		int k = 1000001;
+		for (int j = queries[i][0]; j <= queries[i][1]; ++j) { // 행 부터 열까지 비교
+			if (arr[j] > queries[i][2] && arr[j] < k) {
+				k = arr[j];
+			}
+		}
+		if (k == 1000001) {
+			answer.emplace_back(-1);
+		}
+		else {
+			answer.emplace_back(k);
+		}
+    }
+    
+    return answer;
+}
