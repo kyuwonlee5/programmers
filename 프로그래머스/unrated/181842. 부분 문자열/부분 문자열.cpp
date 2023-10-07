f134fe411519e65516f4f81e14607a087378ea98
kyuwonlee5 <75664301+kyuwonlee5@users.noreply.github.com>
2023-10-07 23:38:18 +0900

    [unrated] Title: 부분 문자열, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/unrated/181842. 부분 문자열/부분 문자열.cpp
+++ 프로그래머스/unrated/181842. 부분 문자열/부분 문자열.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int solution(string str1, string str2) {
     int answer = str2.find(str1);
-    if(answer != -1) {
+    if(answer != -1) { // find 함수에서는 찾는 문자가 없을때 -1로 반환
         answer = 1;
     } else {
         answer = 0;
