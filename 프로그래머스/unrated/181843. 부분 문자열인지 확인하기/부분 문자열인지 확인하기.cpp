6ced5d5dea5748fcfdae16e24ff5be5bc0b5c469
kyuwonlee5 <75664301+kyuwonlee5@users.noreply.github.com>
2023-10-07 23:32:58 +0900

    [unrated] Title: 부분 문자열인지 확인하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string my_string, string target) {
+    int answer = 0;
+    if(my_string.find(target) != -1) {
+        answer = 1;
+    } else {
+        answer = 0;
+    }
+    return answer;
+}
