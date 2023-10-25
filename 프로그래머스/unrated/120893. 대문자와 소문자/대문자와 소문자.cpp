76c9a118af8de1656f6670a75c36735ca1cc3df9
kyuwonlee5 <75664301+kyuwonlee5@users.noreply.github.com>
2023-10-25 22:38:39 +0900

    [level unrated] Title: 대문자와 소문자, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120893. 대문자와 소문자/대문자와 소문자.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string) {
+    for(int i = 0; i < my_string.size(); ++i) {
+        if(isupper(my_string[i])) {
+            my_string[i] = tolower(my_string[i]);
+        } else {
+            my_string[i] = toupper(my_string[i]);
+        }
+    }
+    return my_string;
+}
